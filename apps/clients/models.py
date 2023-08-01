b1ac7fa777b15f586a34bef5785d0f958af59933
Pasindu-97 <pasindusandaruwan1997510@gmail.com>
2023-08-01 16:45:22 +0530

    Change the id generation

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from model_utils.models import TimeStampedModel, UUIDModel
+from model_utils.models import TimeStampedModel
 
 
-class Client(UUIDModel, TimeStampedModel, models.Model):
-    id = models.CharField(_("Id"), max_length=10, unique=True, primary_key=True)
+class Client(TimeStampedModel, models.Model):
     first_name = models.CharField(_("First Name"), max_length=127)
     last_name = models.CharField(_("Last Name"), max_length=127)
     nic = models.CharField(_("NIC"), max_length=15)
@@ -13,24 +12,10 @@ class Client(UUIDModel, TimeStampedModel, models.Model):
     phone_number = models.CharField(_("Phone Number"), max_length=15)
     route = models.CharField(_("Route"), max_length=63, null=True, blank=True)
 
-    def generate_id(self):
-        latest_client = Client.objects.order_by("-id").first()
-        if latest_client:
-            latest_id_num = int(latest_client.id[2:])
-            new_id_num = latest_id_num + 1
-        else:
-            new_id_num = 1
-        return f"LM{new_id_num}"
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.id = self.generate_id()
-        super().save(*args, **kwargs)
-
     class Meta:
         ordering = ["id"]
         verbose_name = _("Client")
         verbose_name_plural = _("Clients")
 
     def __str__(self):
-        return str(self.id)
+        return f"LM{self.id}"
