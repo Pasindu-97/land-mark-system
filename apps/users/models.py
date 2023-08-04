3a98d8fc77c88e2ccc3416cfbdeb50d137840321
Pasindu-97 <pasindusandaruwan1997510@gmail.com>
2023-08-04 22:50:18 +0530

    Create all models

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,15 +4,17 @@ from django.utils.translation import gettext_lazy as _
 from model_utils.models import UUIDModel
 
 
-class User(UUIDModel, models.Model, AbstractUser):
+class User(UUIDModel, AbstractUser):
     class Types(models.TextChoices):
         ADMIN = "ADMIN", _("Admin")
         OFFICE_STAFF = "OFFICE_STAFF", _("Office Staff")
         COLLECTOR = "COLLECTOR", _("Collector")
         INVESTOR = "INVESTOR", _("Investor")
 
-    nic = models.CharField(_("NIC"), max_length=15)
-    type = models.CharField(_("Type"), max_length=50, choices=Types.choices, default=Types.OFFICE_STAFF)
+    nic = models.CharField(_("NIC"), max_length=15, null=True, blank=True)
+    type = models.CharField(
+        _("Type"), max_length=50, choices=Types.choices, default=Types.OFFICE_STAFF, null=True, blank=True
+    )
     address = models.CharField(_("Address"), max_length=1023, null=True, blank=True)
 
     class Meta:
